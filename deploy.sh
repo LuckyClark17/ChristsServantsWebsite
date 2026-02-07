0af1567c85a633fbb88c4f6def8905a12a9d00a6
LuckyClark17 <113465840+LuckyClark17@users.noreply.github.com>
2026-02-06 20:22:30 -0600

    created deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+set -e
+
+cd Backend
+
+echo "Building and Deploying SAM"
+sam build && sam deploy 
+
+echo "Re-Deploying Frontend:"
+aws amplify start-job --app-id d1y0l8029swz9t --branch-name main --job-type RELEASE
+
+echo "All tasks dispatched succesfully"
