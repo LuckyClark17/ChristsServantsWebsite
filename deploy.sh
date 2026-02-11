a004cf8d68c4dfd8d46cb03a10bffd21f8a01246
LuckyClark17 <113465840+LuckyClark17@users.noreply.github.com>
2026-02-10 21:03:23 -0600

    refactored template.yaml into specific templates

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e
 cd Backend
 
 echo "Building and Deploying SAM"
-sam build && sam deploy 
+sam build && sam deploy
 
 echo "Re-Deploying Frontend:"
 aws amplify start-job --app-id d1y0l8029swz9t --branch-name main --job-type RELEASE
