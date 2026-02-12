e686b7e598183f929b24ae304756f0087ba7e686
LuckyClark17 <113465840+LuckyClark17@users.noreply.github.com>
2026-02-12 12:20:22 -0600

    template and deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,17 @@ set -e
 
 cd Backend
 
-echo "Building and Deploying SAM"
-sam build && sam deploy
+echo "Building and Deploying Network Stack"
+sam build -t network-template.yaml && sam deploy -t network-template.yaml --stack-name cs-network-stack
+
+echo "Building and Deploying IAM Stack"
+sam build -t iam-template.yaml && sam deploy -t iam-template.yaml --stack-name cs-iam-stack
+
+echo "Building and Deploying Data Stack"
+sam build -t data-template.yaml && sam deploy -t data-template.yaml --stack-name cs-data-stack
+
+echo "Building and Deploying API Stack"
+sam build -t api-template.yaml && sam deploy -t api-template.yaml --stack-name cs-api-stack
 
 echo "Re-Deploying Frontend:"
 aws amplify start-job --app-id d1y0l8029swz9t --branch-name main --job-type RELEASE
