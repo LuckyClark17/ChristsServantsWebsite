a7a3d3f3eb7c94a894f624f1e2865b4b15737d0e
LuckyClark17 <113465840+LuckyClark17@users.noreply.github.com>
2026-02-12 13:02:06 -0600

    fixed stack-name ref in amplify.yml

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@ set -e
 cd Backend
 
 echo "Building and Deploying Network Stack"
-sam build -t network-template.yaml && sam deploy -t network-template.yaml --stack-name cs-network-stack
+sam build -t network-template.yaml && sam deploy -t network-template.yaml --stack-name cs-network-stack --no-confirm-changeset --no-fail-on-empty-changeset
 
 echo "Building and Deploying IAM Stack"
-sam build -t iam-template.yaml && sam deploy -t iam-template.yaml --stack-name cs-iam-stack
+sam build -t iam-template.yaml && sam deploy -t iam-template.yaml --stack-name cs-iam-stack --no-confirm-changeset --no-fail-on-empty-changeset
 
 echo "Building and Deploying Data Stack"
-sam build -t data-template.yaml && sam deploy -t data-template.yaml --stack-name cs-data-stack
+sam build -t data-template.yaml && sam deploy -t data-template.yaml --stack-name cs-data-stack --no-confirm-changeset --no-fail-on-empty-changeset
 
 echo "Building and Deploying API Stack"
-sam build -t api-template.yaml && sam deploy -t api-template.yaml --stack-name cs-api-stack
+sam build -t api-template.yaml && sam deploy -t api-template.yaml --stack-name cs-api-stack --no-confirm-changeset --no-fail-on-empty-changeset
 
 echo "Re-Deploying Frontend:"
 aws amplify start-job --app-id d1y0l8029swz9t --branch-name main --job-type RELEASE
